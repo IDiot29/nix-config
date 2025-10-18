8914fbae09721cdc6b41850586c6158ef64a78d7
vldo766hi <rivaldo.silalahi@lintasarta.co.id>
2025-10-19 02:13:42 +0700

    create fish function for source .env files with bash format

--- home-manager/fish.nix
+++ home-manager/fish.nix
@@ -15,6 +15,39 @@
       if test -f ~/.config/fish/secrets.fish
         source ~/.config/fish/secrets.fish
       end
+
+      # Function to source bash-style .env files
+      function envsource
+        if test (count $argv) -eq 0; or test "$argv[1]" = "--help"
+          echo "Usage: envsource <file>"
+          echo ""
+          echo "Source bash-style environment files in fish shell"
+          echo ""
+          echo "Example:"
+          echo "  envsource .env"
+          echo "  envsource /path/to/my-vars.env"
+          echo ""
+          echo "Supports formats:"
+          echo "  KEY=value"
+          echo "  export KEY=value"
+          echo '  KEY="value"'
+          echo "  KEY='value'"
+          return 0
+        end
+
+        set -l envfile $argv[1]
+        if not test -f $envfile
+          echo "Error: File '$envfile' not found"
+          return 1
+        end
+
+        for line in (cat $envfile | grep -v '^#' | grep -v '^$')
+          set item (string split -m 1 '=' -- $line)
+          set -l value (string trim --chars='\'"' -- $item[2])
+          set -gx $item[1] $value
+        end
+        echo "Sourced $envfile"
+      end
     '';
     
     shellAliases = {
@@ -36,8 +69,6 @@
       la = "ez -l --all";
       
       # Shortcuts
-      nv = "neovim";
-      vim = "neovim";
       ff = "fastfetch";
       
       # Zoxide
