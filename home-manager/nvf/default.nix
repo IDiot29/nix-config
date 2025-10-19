a28717b0289319feb434da63b3def4e3b99275ac
vldo766hi <rivaldo.silalahi@lintasarta.co.id>
2025-10-19 22:08:01 +0700

    adding config color for lazygit

--- home-manager/nvf/default.nix
+++ home-manager/nvf/default.nix
@@ -10,4 +10,39 @@ in {
     defaultEditor = true;
     settings = nvfSettings;
   };
+
+  # Lazygit Catppuccin Mocha theme
+  xdg.configFile."lazygit/config.yml" = {
+    force = true;
+    text = ''
+      gui:
+        theme:
+          activeBorderColor:
+            - '#89b4fa' # Blue
+            - bold
+          inactiveBorderColor:
+            - '#a6adc8' # Subtext0
+          searchingActiveBorderColor:
+            - '#f9e2af' # Yellow
+            - bold
+          optionsTextColor:
+            - '#89b4fa' # Blue
+          selectedLineBgColor:
+            - '#313244' # Surface0
+          selectedRangeBgColor:
+            - '#313244' # Surface0
+          cherryPickedCommitBgColor:
+            - '#45475a' # Surface1
+          cherryPickedCommitFgColor:
+            - '#89b4fa' # Blue
+          unstagedChangesColor:
+            - '#f38ba8' # Red
+          defaultFgColor:
+            - '#cdd6f4' # Text
+      git:
+        paging:
+          colorArg: always
+          pager: delta --dark --paging=never
+    '';
+  };
 }
