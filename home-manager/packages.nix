c2635842473134f90371e479978e7d1bcd5f2eb5
vldo766hi <rivaldo.silalahi@lintasarta.co.id>
2025-10-20 13:20:03 +0700

    adding sops package

--- home-manager/packages.nix
+++ home-manager/packages.nix
@@ -1,41 +1,45 @@
 # My packages
-{ config, pkgs, inputs, ... }:
-
 {
+  config,
+  pkgs,
+  inputs,
+  ...
+}: {
   home.packages = with pkgs; [
     # CLI
-    neovim          
-    git             
-    nil             
-    nixpkgs-fmt     
-    nodejs         
-    ripgrep        
-    bat           
-    btop           
-    fastfetch      
-    eza            
-    fd              
-    fzf             
-    zoxide          
+    neovim
+    git
+    nil
+    nixpkgs-fmt
+    nodejs
+    ripgrep
+    bat
+    btop
+    fastfetch
+    eza
+    fd
+    fzf
+    zoxide
     atuin
     kubectl
     kustomize
-    flux
+    fluxcd
     dig
     helm
-    
+    sops
+
     # GUI
-    alacritty       
-    fuzzel          
-    obs-studio      
-    mpv              
+    alacritty
+    fuzzel
+    obs-studio
+    mpv
     pritunl-client
     keepassxc
-    
+
     # Wayland
-    swaylock    
+    swaylock
     xwayland-satellite
-    
+
     # Zed - Using prebuild from github release
     (pkgs.callPackage ./zed.nix {})
   ];
