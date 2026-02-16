8c1a4ade97bb0a9137e66a5ebf0b3dddd08ec940
valdo766hi <rivaldosilalahi13@gmail.com>
2026-02-16 11:30:02 +0700

    fix integration font and shell on kitty

--- home-manager/common/kitty.nix
+++ home-manager/common/kitty.nix
@@ -8,7 +8,11 @@
     themeFile = "Catppuccin-Mocha";
     font = {
       name = "JetBrains Mono";
+      package = pkgs.jetbrains-mono;
     };
     shellIntegration.enableFishIntegration = true;
+    settings = {
+      shell = lib.getExe pkgs.fish;
+    };
   };
 }
