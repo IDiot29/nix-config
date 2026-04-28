88d350901b5e997218f9c791a23257c56e10eec8
valdo766hi <rivaldosilalahi13@gmail.com>
2026-04-29 01:05:42 +0700

    update rtk version

--- pkgs/rtk/default.nix
+++ pkgs/rtk/default.nix
@@ -4,15 +4,15 @@
   fetchurl,
 }:
 let
-  version = "0.30.0";
+  version = "0.37.02";
   sources = {
     x86_64-linux = {
       url = "https://github.com/rtk-ai/rtk/releases/download/v${version}/rtk-x86_64-unknown-linux-musl.tar.gz";
-      sha256 = "4bc9f340842b1948bd7e9348e2a1a9cced1b39d2cbb2687376ef9b436aa2fcee";
+      sha256 = "3dfb7a05636a68687ba1c5aa696fa8d5fcb494447ded86d9eb8b88b7100a37c6";
     };
     aarch64-darwin = {
       url = "https://github.com/rtk-ai/rtk/releases/download/v${version}/rtk-aarch64-apple-darwin.tar.gz";
-      sha256 = "0c7a47c6d65f4bf2222170f825e2c9fb9f59109ac9ce16a89c9bcdc0f6606499";
+      sha256 = "99e20a59847dedbb64032a3f7985f2fe959fcb9674d8eaf940fc58a189e27eca";
     };
   };
   source =
