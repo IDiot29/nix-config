c2a70069069a857e42ba20e354c5a3669d7904ca
valdo766hi <rivaldosilalahi13@gmail.com>
2026-07-19 02:38:13 +0700

    add mcp

--- home-manager/common/pi.nix
+++ home-manager/common/pi.nix
@@ -1,4 +1,18 @@
 {...}: {
+  xdg.configFile."mcp/mcp.json".text = builtins.toJSON {
+    mcpServers = {
+      context7 = {
+        url = "https://mcp.context7.com/mcp";
+        headers.CONTEXT7_API_KEY = "\${CONTEXT7_API_KEY}";
+      };
+
+      exa = {
+        url = "https://mcp.exa.ai/mcp";
+        headers."x-api-key" = "\${EXA_API_KEY}";
+      };
+    };
+  };
+
   programs.pi-coding-agent = {
     enable = true;
     package = null;
@@ -12,6 +26,7 @@
       packages = [
         "npm:cc-safety-net@1.0.6"
         "npm:pi-lens@3.8.70"
+        "npm:pi-mcp-adapter@2.11.0"
       ];
     };
   };
