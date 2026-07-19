726ada67982922b4d21e1a9d282a061e29f1548b
valdo766hi <rivaldosilalahi13@gmail.com>
2026-07-20 00:33:49 +0700

    add trusted devenv

--- modules/darwin/common/default.nix
+++ modules/darwin/common/default.nix
@@ -31,11 +31,13 @@
         "https://cache.nixos.org"
         "https://vicinae.cachix.org"
         "https://niri.cachix.org"
+        "https://devenv.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
         "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
+        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       ];
     };
   };
--- modules/nixos/common/default.nix
+++ modules/nixos/common/default.nix
@@ -21,11 +21,13 @@
         "https://cache.nixos.org"
         "https://niri.cachix.org"
         "https://vicinae.cachix.org"
+        "https://devenv.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
         "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
+        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       ];
     };
     gc = {
