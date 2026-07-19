7591aa71fb816d0e3b027e3c96a720529814f7e4
valdo766hi <rivaldosilalahi13@gmail.com>
2026-07-20 00:40:38 +0700

    edit allow with direct edit nix.custom.conf

--- modules/darwin/common/default.nix
+++ modules/darwin/common/default.nix
@@ -31,16 +31,19 @@
         "https://cache.nixos.org"
         "https://vicinae.cachix.org"
         "https://niri.cachix.org"
-        "https://devenv.cachix.org"
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
         "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
-        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
       ];
     };
   };
+
+  environment.etc."nix/nix.custom.conf".text = ''
+    extra-substituters = https://devenv.cachix.org
+    extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
+  '';
 
   environment.systemPackages = with pkgs; [
     devenv
