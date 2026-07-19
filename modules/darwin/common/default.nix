ce8074089bbe73f2d361a80ca1748c5eeefafbd8
valdo766hi <rivaldosilalahi13@gmail.com>
2026-07-20 00:53:05 +0700

    update nix allow trusted

--- modules/darwin/common/default.nix
+++ modules/darwin/common/default.nix
@@ -18,31 +18,13 @@
 
   nixpkgs.hostPlatform = "aarch64-darwin";
 
-  nix = {
-    enable = false;
-    settings = {
-      store = "daemon";
-      experimental-features = "nix-command flakes";
-      trusted-users = [
-        "root"
-        "rivaldo"
-      ];
-      substituters = [
-        "https://cache.nixos.org"
-        "https://vicinae.cachix.org"
-        "https://niri.cachix.org"
-      ];
-      trusted-public-keys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc="
-        "niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964="
-      ];
-    };
-  };
+  nix.enable = false;
 
+  # Determinate Nix owns nix.conf because nix.enable is false.
   environment.etc."nix/nix.custom.conf".text = ''
-    extra-substituters = https://devenv.cachix.org
-    extra-trusted-public-keys = devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
+    accept-flake-config = true
+    extra-substituters = https://vicinae.cachix.org https://niri.cachix.org https://devenv.cachix.org
+    extra-trusted-public-keys = vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc= niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964= devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
   '';
 
   environment.systemPackages = with pkgs; [
