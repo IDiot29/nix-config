8fa307efac6aba32ae1399597e49801414b7aa31
valdo766hi <rivaldosilalahi13@gmail.com>
2026-07-24 09:27:59 +0700

    add rivaldo as trusted user

--- modules/darwin/common/default.nix
+++ modules/darwin/common/default.nix
@@ -22,6 +22,7 @@
 
   # Determinate Nix owns nix.conf because nix.enable is false.
   environment.etc."nix/nix.custom.conf".text = ''
+    trusted-users = root rivaldo
     accept-flake-config = true
     extra-substituters = https://vicinae.cachix.org https://niri.cachix.org https://devenv.cachix.org
     extra-trusted-public-keys = vicinae.cachix.org-1:1kDrfienkGHPYbkpNj1mWTr7Fm1+zcenzgTizIcI3oc= niri.cachix.org-1:Wv0OmO7PsuocRKzfDoJ3mulSl7Z6oezYhGhR+3W2964= devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
