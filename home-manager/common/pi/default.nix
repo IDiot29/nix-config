006c5af0883d58bc8e48965dee1e33b6c37313f8
valdo766hi <rivaldosilalahi13@gmail.com>
2026-08-17 17:07:19 +0700

    update pi-fast and add pi-lazy-skill-tool

--- home-manager/common/pi/default.nix
+++ home-manager/common/pi/default.nix
@@ -120,9 +120,10 @@ in {
         "npm:@plannotator/pi-extension@0.27.1"
         "npm:pi-subagents@0.48.0"
         "npm:@gotgenes/pi-permission-system@25.1.0"
-        "npm:@valdo766hi/pi-fast@0.1.0"
+        "npm:@valdo766hi/pi-fast@0.1.2"
         "npm:@valdo766hi/pi-footer@0.1.0"
         "npm:@valdo766hi/pi-yolo@0.1.0"
+        "npm:@valdo766hi/pi-lazy-skill-tool@0.1.1"
         "npm:@mtrojnar/pi-usage@0.1.4"
       ];
     };
