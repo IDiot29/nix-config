f9601b43e3a9dcc9652fa2b58e13784805c33dc8
valdo766hi <rivaldosilalahi13@gmail.com>
2026-08-18 23:12:14 +0700

    update pi yolo to 0.1.2

--- home-manager/common/pi/default.nix
+++ home-manager/common/pi/default.nix
@@ -122,7 +122,7 @@ in {
         "npm:@gotgenes/pi-permission-system@25.1.0"
         "npm:@valdo766hi/pi-fast@0.1.2"
         "npm:@valdo766hi/pi-footer@0.1.0"
-        "npm:@valdo766hi/pi-yolo@0.1.0"
+        "npm:@valdo766hi/pi-yolo@0.1.2"
         "npm:@valdo766hi/pi-lazy-skill-tool@0.1.1"
         "npm:@mtrojnar/pi-usage@0.1.4"
       ];
