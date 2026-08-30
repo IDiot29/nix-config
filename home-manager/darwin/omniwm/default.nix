dd50d38538283f0a6405c755aaf11cfbce548ec8
valdo766hi <rivaldosilalahi13@gmail.com>
2026-08-31 06:33:28 +0700

    update omniwm config

--- home-manager/darwin/omniwm/default.nix
+++ home-manager/darwin/omniwm/default.nix
@@ -6,8 +6,10 @@
   home.activation.omniwmSettings = lib.hm.dag.entryAfter ["writeBoundary"] ''
     settings_dir="$HOME/.config/omniwm"
     ${pkgs.coreutils}/bin/mkdir -p "$settings_dir"
-    ${pkgs.coreutils}/bin/install -m 600 \
-      ${./settings.toml} "$settings_dir/settings.toml"
+    settings_file="$settings_dir/settings.toml"
+    settings_tmp="$settings_file.nix-tmp"
+    ${pkgs.coreutils}/bin/install -m 600 ${./settings.toml} "$settings_tmp"
+    ${pkgs.coreutils}/bin/mv -f "$settings_tmp" "$settings_file"
 
     /usr/bin/defaults write -g NSConvolutionOverride1 -float 8.0
     /usr/bin/defaults write -g NSConvolutionOverride2 -float 8.0
